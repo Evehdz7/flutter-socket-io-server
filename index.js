1c6f0b4686e10f990e8a511bfaab236a4b6648c6
Evehdz7 <byhdz7@gmail.com>
2022-02-21 20:53:36 -0600

    Seccion 3 Curso Flutter Avenzado

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+require('dotenv').config();
+
+//---------APP express----------
+const app = express();
+
+//---------NODE SERVER----------
+const server = require('http').createServer(app);
+module.exports.io = require('socket.io')(server); 
+require('./sockets/socket');
+
+
+
+//---------path public----------
+const publicPath = path.resolve( __dirname, 'public' );
+app.use( express.static( publicPath ) );
+
+server.listen( process.env.PORT, (err) => {
+    
+    if(err) throw new Error(err);
+
+    console.log('Servidor corriendo en puerto', process.env.PORT);
+    
+});
